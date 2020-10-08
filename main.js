99c1b9dc4b1c16be3e6b0095f01b1354517b6e9a
TanyaLark <54994530+TanyaLark@users.noreply.github.com>
2020-10-08 20:41:01 +0300

    brackets changed

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-let grossIncome; 
 const button = document.getElementById('taxButton');
 button.addEventListener('click', function () {
-    grossIncome = document.getElementById('inputTax').value;
+    const grossIncome = document.getElementById('inputTax').value;
     const deductions = 12400; //$, the standard deduction for 2021 
     let AGI = grossIncome - deductions;//$, adjusted gross income (AGI)
     const result = taxСalculator(grossIncome, AGI);
@@ -9,22 +8,22 @@ button.addEventListener('click', function () {
 });
 
 //2020 Tax Brackets (Due April 2021)
-const maxBracketTax10 = 9875;//$
+const maxBracketTax10 = 9875.99;//$
 
 const minBracketTax12 = 9876;//$
-const maxBracketTax12 = 40125;//$
+const maxBracketTax12 = 40125.99;//$
 
 const minBracketTax22 = 40126;//$
-const maxBracketTax22 = 85525;//$
+const maxBracketTax22 = 85525.99;//$
 
 const minBracketTax24 = 85526;//$
-const maxBracketTax24 = 163300;//$
+const maxBracketTax24 = 163300.99;//$
 
 const minBracketTax32 = 163301;//$
-const maxBracketTax32 = 207350;//$
+const maxBracketTax32 = 207350.99;//$
 
 const minBracketTax35 = 207351;//$
-const maxBracketTax35 = 518400;//$
+const maxBracketTax35 = 518400.99;//$
 
 const minBracketTax37 = 518401;//$
 
